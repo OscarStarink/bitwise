f935bd1e3cb337cf0829ff33e3502217c6fe9612
Oscar Starink <oscar.starink@gmail.com>
2018-08-06 22:20:43 +0200

    Implemented [ and ], brainfuck is now fully supported

--- rattle/brainfuck.py
+++ rattle/brainfuck.py
@@ -27,7 +27,7 @@ def add(x, y, c=0):
     return s
 
 @module
-class Decode:
+class Decoder:
 	opt = input(bit[8])
 	
 	inc = output( opt == ord('+') )
@@ -38,6 +38,9 @@ class Decode:
 	
 	putc = output( opt == ord('.') )
 	getc = output( opt == ord(',') )
+	
+	loop = output( opt == ord('[') )
+	lend = output( opt == ord(']') )
 
 @module
 class Incrementor:
@@ -46,6 +49,7 @@ class Incrementor:
 	inc = input(bit)
 	dec = input(bit)
 	
+	# Implement fancy counter here
 	val = when( inc | dec, i + when(inc, bit[N](1), bit[N](-1) ) , i )
 
 	o = output(val)
@@ -61,27 +65,47 @@ class Top:
 	rx_valid = input(bit)
 	rx_in = input(bit[8])
 	
-	decode = Decode(opt = opt)
+	seek_back_in = input(bit)
+	seek_forward_in = input(bit)
+	interpret_in = ~seek_back_in  & ~seek_forward_in
+	
+	brace_in = input(bit[8]) 
+	
+	decode = Decoder(opt = opt)
 	
 	stall = decode.putc & ~tx_ready | decode.getc & ~rx_valid
 	
-	sp = Incrementor(inc=decode.right, dec=decode.left, i=sp_in)
+	mem_null = mem_in == 0
+	brace_null = brace_in == 0
+	
+	seek_forward = mem_null & interpret_in & decode.loop | seek_forward_in & ~(brace_null & decode.lend)
+	seek_back = ~mem_null & interpret_in & decode.lend | seek_back_in & ~(brace_null & decode.loop)
+	
+	interpret = ~seek_back & ~seek_forward
+	
+	mem = when(decode.getc & rx_valid, rx_in, mem_in)
+	
+	sp = Incrementor(inc=decode.right & interpret, dec=decode.left&interpret, i=sp_in)
+	ip = Incrementor(inc=~stall&interpret | seek_forward, i=ip_in, dec=seek_back ) 	
+	alu = Incrementor(inc = decode.inc&interpret, dec = decode.dec&interpret, i=mem)
+	
+
+	# TODO ALU could be used to update brace counter
+	brace = Incrementor( i=brace_in, inc=seek_forward & decode.loop | seek_back & decode.lend, dec=seek_forward_in & decode.lend | seek_back_in & decode.loop )
 	
 	
+	seek_back_out = output(seek_back)
+	seek_forward_out = output(seek_forward)
 	
-	ip = Incrementor( inc=~stall, i=ip_in, dec=False) 
 	
-	alu = Incrementor(inc = decode.inc, dec = decode.dec, i = mem_in) 
+	rx_ready = output(decode.getc & interpret)
+	tx_valid = output(decode.putc & interpret)
+	tx_out = output(mem_in)
 	
-	mem = when(decode.getc & rx_valid, rx_in, alu.o)
-	
-	rx_ready = output( decode.getc )
-	tx_valid = output( decode.putc )
-	tx_out = output( mem_in )
-	
-	ip_out = output(ip.o )
-	sp_out = output(sp.o )
-	mem_out = output(mem)
+	brace_out = output(brace.o)
+	ip_out = output(ip.o)
+	sp_out = output(sp.o)
+	mem_out = output(alu.o)
 	
 	
 open('example.dot', 'w').write(generate_dot_file(Top))
@@ -98,12 +122,18 @@ class Sim:
 		self.kwargs = kwargs
 	
 	def reset(self):
+		self.i = 1
 		self.top.ip_out = 0
 		self.top.sp_out = 0
 		self.top.mem_out = 0
 		self.stack = [0]*256
 		self.stdout = []
 		self.top.tx_ready = True
+		
+		self.top.brace_out = -1
+		self.top.seek_forward_out = False
+		self.top.seek_back_out = False
+		
 		
 	def steps(self, n):
 		for _ in range(n):
@@ -115,11 +145,27 @@ class Sim:
 		self.top.mem_in = self.stack[self.top.sp_out]
 		self.top.opt = self.prog[self.top.ip_out]
 		
+		self.top.seek_forward_in = self.top.seek_forward_out
+		self.top.seek_back_in = self.top.seek_back_out
+		self.top.brace_in = self.top.brace_out
 		
 		self.top.rx_in = self.stdin[0] if len(self.stdin) else 0
 		self.top.rx_valid = len(self.stdin) > 0
 		
+		if False:
+			mode = 'interpret'
+			if self.top.seek_forward_in:
+				mode = 'forward'
+			if self.top.seek_back_in:
+				mode = 'back'
+		
+			print("in {} {} {} {}".format(self.i, chr(self.top.opt), self.top.brace_in, mode) )
+		
+		
 		self.top.update(**self.kwargs)
+		
+		
+		self.i = self.i + 1
 		
 		self.stack[self.top.sp_in] = self.top.mem_out
 		if self.top.tx_valid and self.top.tx_ready:
@@ -162,11 +208,55 @@ def test_read():
 	assert sim.stdout == list(reversed(stdin))
 	
 
+def test_loop_cond():
+	sim = Sim(top, [ ord(x) for x in "+[.-]." ] )
+	sim.steps(2)
+	assert sim.top.seek_forward_out == False
+	sim.steps(3)
+	assert sim.top.seek_back_out == False
+	sim.step()
+	assert sim.stdout == [1, 0]
+	
+def test_loop_skip():
+	sim = Sim(top, [ ord(x) for x in "[.]." ] )
+	sim.step()
+	assert sim.top.seek_forward_out == True
+	sim.steps(3)
+	assert sim.stdout == [0]
+	assert sim.top.brace_out == 255
+	
+def test_loop_rewind():
+	sim = Sim(top, [ ord(x) for x in "+++[.-]." ] )
+	sim.steps(7)
+	assert sim.top.seek_back_out == True
+	sim.steps(4)
+	assert sim.stdout == [3, 2]
+	sim.steps(6)
+	assert sim.stdout == [3, 2, 1]
+	sim.steps(3)
+	assert sim.stdout == [3, 2, 1, 0]
+	assert sim.top.brace_out == 255
+
+def test_loop_nested():
+	sim = Sim(top, [ ord(x) for x in "+++[>+++[>+++<-]<-]>>." ] )
+	sim.steps(166)
+	assert sim.stdout == [3*3*3]
+
+def test_hello_world():
+	sim = Sim(top, [ ord(x) for x in "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-."])
+	sim.steps(22704)
+	assert sim.stdout == [ord(x) for x in "hello world"]
+
 def test():
 	test_add_sub()
 	test_move()
 	test_print()
 	test_read()
-
+	test_loop_cond()
+	test_loop_skip()
+	test_loop_rewind()
+	test_loop_nested()
+	test_hello_world()
+	
 test()
 
