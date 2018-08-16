a1630dcc7f69155e80a4c0758ca44db0e1d440b4
Oscar Starink <oscar.starink@gmail.com>
2018-08-17 00:24:55 +0200

    Added instruction @ and #, and cleaned up test code. Started using '.out' for outputs. Tried to use native memory, but that triggers some bug in rattle.

--- rattle/brainfuck.py
+++ rattle/brainfuck.py
@@ -1,20 +1,25 @@
 from rattle import *
+import unittest
+
+#TODO
+# Use register() memory and clean up mem interface
+# Use generics for CPU word width and address width
+# Improve simulation
+
 
 @module
 class Decoder:
 	opt = input(bit[8])
-	
-	inc = output( opt == ord('+') )
-	dec = output( opt == ord('-') )
-	
+	inc  = output( opt == ord('+') )
+	dec  = output( opt == ord('-') )
 	left = output( opt == ord('<') )
-	right = output( opt == ord('>') )
-	
+	right= output( opt == ord('>') )
 	putc = output( opt == ord('.') )
 	getc = output( opt == ord(',') )
-	
 	loop = output( opt == ord('[') )
 	lend = output( opt == ord(']') )
+	brk  = output( opt == ord('#') )
+	halt = output( opt == ord('@') )
 
 @module
 class Incrementor:
@@ -30,25 +35,40 @@ class Incrementor:
 
 
 @module
-class Top:
-	opt = input(bit[8])
-	mem_in = input(bit[8])
-	
+class CPU:
+	addr_width = 8
+
 	tx_ready = input(bit)
+	tx_valid = output(bit)
+	tx_out = output(bit[8])
+
+	rx_ready = output(bit)
 	rx_valid = input(bit)
 	rx_in = input(bit[8])
-	
-	ip = register(bit[8])
-	sp = register(bit[8])
+
+	cont = input(bit)
+	brk = output(bit)
+	halt = output(bit)
+
+	opt_addr = output(bit[addr_width])
+	opt = input(bit[8])
+
+	mem_addr = output(bit[addr_width])
+	mem_out = output(bit[8])
+	mem_in = input(bit[8])
+	mem_write = output(bit)
+
+	ip = register(bit[addr_width])
+	sp = register(bit[addr_width])
 	brace = register(bit[8])
 
 	seek_back = register(bit)
 	seek_forward = register(bit)
 	interpret = ~seek_back  & ~seek_forward
-		
+
 	decode = Decoder(opt = opt)
 	
-	stall = decode.putc & ~tx_ready | decode.getc & ~rx_valid
+	stall = output( decode.putc & ~tx_ready | decode.getc & ~rx_valid | decode.halt | decode.brk & ~cont )
 	
 	mem_null = mem_in == 0
 	brace_null = brace == 0
@@ -71,152 +91,238 @@ class Top:
 	brace.next = brace_inc.o
 	ip.next = ip_inc.o
 	sp.next = sp_inc.o
-	
-	rx_ready = output(decode.getc & interpret)
-	tx_valid = output(decode.putc & interpret)
-	tx_out = output(mem_in)
-	ip_out = output(ip)
-	sp_out = output(sp)
-	mem_out = output(alu.o)
 
+	brk.out = decode.brk & ~cont & interpret
+	halt.out = decode.halt & interpret
 
-open('example.dot', 'w').write(generate_dot_file(Top))
+	rx_ready.out = decode.getc & interpret
+	tx_valid.out = decode.putc & interpret
+	tx_out.out = mem_in
 
+	opt_addr.out = ip
+	mem_addr.out = sp # sp_inc.o
+	mem_out.out = alu.o
+	mem_write.out = decode.inc | decode.dec | (decode.getc & rx_valid)
 
-top = compile(Top, trace_all=True)
+#def mux(addr, data):
+#    assert ispow2(len(data))
+#    if len(data) == 1:
+#        return data[0]
+#    else:
+#        i = len(data) // 2
+#        return when(addr[-1], mux(addr[:-1], data[i:]), mux(addr[:-1], data[:i]))
+
+#@module
+#def memory(data_type, size):
+#	assert ispow2(size)
+#	addr_type = bit[clog2(size)]
+#	write_enable = input(bit)
+#	write_addr = input(addr_type)
+#	write_data = input(data_type)
+
+#	read_addr = input(addr_type)
+
+#	# NOTE Unfortunately rattle doesn't see the array, and creates N anonymous registers.
+#	cells = [ register(data_type) for i in range(size) ]
+#	# work around
+#	for i, cell in enumerate(cells):
+#		cell.name = "cells_{}".format(i)
+
+#	# Read-after-write semantics: Bypass write data to read data if addresses match.
+#	read_data = output(delay(when(write_enable & (read_addr == write_addr), write_data, mux(read_addr, cells))))
+
+#	for i, cell in enumerate(cells):
+#		cell.next = when(write_enable & (write_addr == i), write_data, cell)
+
+#@module
+#class Top:
+#	tx_ready = input(bit)
+#	tx_valid = output(bit)
+#	tx_out = output(bit[8])
+
+#	rx_ready = output(bit)
+#	rx_valid = input(bit)
+#	rx_in = input(bit[8])
+
+#	opt_addr = output(bit[8])
+#	opt = input(bit[8])
+
+#	stack = memory(bit[8], 4)()
+#	cpu = CPU(mem_in=stack.read_data, tx_ready=tx_ready, rx_valid=rx_valid, rx_in=rx_in, opt=opt)
+
+#	stack.write_data = cpu.mem_out
+#	stack.write_enable = cpu.mem_write
+#	stack.write_addr = cpu.mem_addr[:2]
+#	stack.read_addr = cpu.mem_addr[:2]
+
+#	tx_valid.out = cpu.tx_valid
+#	tx_out.out   = cpu.tx_out
+#	rx_ready.out = cpu.rx_ready
+#	opt_addr.out = cpu.opt_addr
 
 class Sim:
 	def __init__(self, cls, prog, stdin=[], **kwargs):
 		self.top = cls()
-		self.prog = prog
+		self.prog = prog + [ord('@')]
 		self.stdin = stdin.copy()
 		self.reset()
 		self.kwargs = kwargs
 	
 	def reset(self):
 		self.i = 1
-		self.top.mem_out = 0
-		self.stack = [0]*256
+		self.stack_buf = [0]*256
 		self.stdout = []
 		self.top.tx_ready = True
 		
 	def steps(self, n):
 		for _ in range(n):
 			self.step()
-	
+
+	def run(self):
+		self.top.cont = 1 #set cont so next time we continue
+		while True:
+			self.step()
+			if self.top.brk:
+				break
+			if self.top.halt:
+				break
+
+	def stack(self, n):
+		#reg = "stack_cells_{}".format(n)
+		#if hasattr(self.top, reg):
+		#	return getattr(self.top, reg)
+		#else:
+		#	raise IndexError
+		return self.stack_buf[n]
+
 	def step(self):
-		self.top.mem_in = self.stack[self.top.sp]
+		self.top.mem_in = self.stack_buf[self.top.sp]
 		self.top.opt = self.prog[self.top.ip]
 		
 		self.top.rx_in = self.stdin[0] if len(self.stdin) else 0
 		self.top.rx_valid = len(self.stdin) > 0
 		
-		if False:
+		dbg = False
+		if dbg:
 			mode = 'interpret'
-			if self.top.seek_forward_in:
+			if self.top.seek_forward:
 				mode = 'forward'
-			if self.top.seek_back_in:
+			if self.top.seek_back:
 				mode = 'back'
-		
-			print("in {} {} {} {}".format(self.i, chr(self.top.opt), self.top.brace, mode) )
-		
+			print("in {} opt:{} sp:{} ip:{} {} {} cont:{}".format(self.i, chr(self.top.opt), self.top.sp, self.top.ip, self.top.brace, mode, self.top.cont) )
 		
 		self.top.update(**self.kwargs)
-		
+
 		self.i = self.i + 1
 		
-		self.stack[self.top.sp] = self.top.mem_out
+		if self.top.mem_write:
+			self.stack_buf[self.top.sp] = self.top.mem_out
 		if self.top.tx_valid and self.top.tx_ready:
 			self.stdout.append(self.top.tx_out)
 			
 		if self.top.rx_valid and self.top.rx_ready:	
 			self.stdin.pop(0)
-			
+
 		self.top.tick()
 
+		# clear continue, so that we dont fall trough the next break
+		self.top.cont = 0
 
-def test_add_sub():
-	sim = Sim(top, [ ord(x) for x in "++-" ], trace=True )
-	sim.steps(2)	
-	assert sim.stack[0] == 2
-	sim.step()
-	assert sim.stack[0] == 1
-	
-def test_move():
-	sim = Sim(top, [ ord(x) for x in ">>+<+" ] )
-	sim.steps(3)
-	assert sim.stack[2] == 1
-	sim.steps(2)
-	assert sim.stack[1] == 1
+		if dbg:
+			print("stall:{} ip':{} break:{} halt:{}".format( self.top.stall, self.top.ip, self.top.brk, self.top.halt))
 
-def test_print():
-	sim = Sim(top, [ ord(x) for x in "++++.+.." ] )
-	sim.steps(7)
-	assert sim.stdout[0] == 4 
-	assert sim.stdout[1] == 5 
-	sim.top.tx_ready = False
-	sim.steps(10)
-	assert len(sim.stdout) == 2
-	sim.top.tx_ready = True
-	sim.step()
-	assert sim.stdout[2] == 5
-	
-def test_read():
-	stdin = [ord(x) for x in "bar"]
-	sim = Sim(top, [ ord(x) for x in ",>,>,.<.<." ], stdin )
-	sim.steps(10)
-	assert sim.stdout == list(reversed(stdin))
-	
 
-def test_loop_cond():
-	sim = Sim(top, [ ord(x) for x in "+[.-]." ] )
-	sim.steps(2)
-	assert sim.top.seek_forward == False
-	sim.steps(3)
-	assert sim.top.seek_back == False
-	sim.step()
-	assert sim.stdout == [1, 0]
-	
-def test_loop_skip():
-	sim = Sim(top, [ ord(x) for x in "[.]." ] )
-	sim.step()
-	assert sim.top.seek_forward == True
-	sim.steps(3)
-	assert sim.stdout == [0]
-	assert sim.top.brace == 0
-	
-def test_loop_rewind():
-	sim = Sim(top, [ ord(x) for x in "+++[.-]." ] )
-	sim.steps(7)
-	assert sim.top.seek_back == True
-	sim.steps(4)
-	assert sim.stdout == [3, 2]
-	sim.steps(6)
-	assert sim.stdout == [3, 2, 1]
-	sim.steps(3)
-	assert sim.stdout == [3, 2, 1, 0]
-	assert sim.top.brace == 0
+class TestCPUBase():
+	def test_halt(self):
+		sim = Sim(self.cpu, list(b""))
+		sim.run()
 
-def test_loop_nested():
-	sim = Sim(top, [ ord(x) for x in "+++[>+++[>+++<-]<-]>>." ] )
-	sim.steps(166)
-	assert sim.stdout == [3*3*3]
+	def test_nop(self):
+		sim = Sim(self.cpu, list(b"   "))
+		sim.run()
 
-def test_hello_world():
-	sim = Sim(top, [ ord(x) for x in "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-."])
-	sim.steps(22704)
-	assert sim.stdout == [ord(x) for x in "hello world"]
+	def test_add(self):
+		sim = Sim(self.cpu, list(b"++" ))
+		sim.run()
+		assert sim.stack(0) == 2
 
-def test():
-	test_add_sub()
-	test_move()
-	test_print()
-	test_read()
-	test_loop_cond()
-	test_loop_skip()
-	test_loop_rewind()
-	test_loop_nested()
-	test_hello_world()
-	
-test()
+	def test_sub(self):
+		sim = Sim(self.cpu, list(b"++-"))
+		sim.run()
+		assert sim.stack(0) == 1
+
+	def test_break(self):
+		sim = Sim(self.cpu, list(b"+#+#+"))
+		sim.run()
+		assert sim.stack(0) == 1
+		sim.run()
+		assert sim.stack(0) == 2
+		sim.run()
+		assert sim.stack(0) == 3
+
+	def test_move(self):
+		sim = Sim(self.cpu, list(b">>+#<+"))
+		sim.run()
+		assert sim.stack(2) == 1
+		sim.run()
+		assert sim.stack(1) == 1
+
+	def test_print(self):
+		sim = Sim(self.cpu, list(b"++++.+.#."))
+		sim.run()
+		assert sim.stdout[0] == 4
+		assert sim.stdout[1] == 5
+		sim.top.tx_ready = False
+		sim.steps(10)
+		assert len(sim.stdout) == 2
+		sim.top.tx_ready = True
+		sim.run()
+		assert sim.stdout[2] == 5
+
+	def test_read(self):
+		stdin = list(b"bar")
+		sim = Sim(self.cpu, list(b",>,>,.<.<."), stdin )
+		sim.run()
+		assert sim.stdout == list(reversed(stdin))
+
+	def test_loop_cond(self):
+		sim = Sim(self.cpu, list(b"+[.-]."))
+		sim.run()
+		assert sim.stdout == [1, 0]
+
+	def test_loop_skip(self):
+		sim = Sim(self.cpu, list(b"[.]."))
+		sim.run()
+		assert sim.stdout == [0]
+		assert sim.top.brace == 0
+
+	def test_loop_rewind(self):
+		sim = Sim(self.cpu, list(b"+++[.-]."))
+		sim.run()
+		assert sim.stdout == [3, 2, 1, 0]
+		assert sim.top.brace == 0
+
+	def test_loop_nested(self):
+		sim = Sim(self.cpu, list(b"+++[>+++[>+++<-]<-]>>."))
+		sim.run()
+		assert sim.stdout == [3*3*3]
+
+	def test_hello_world(self):
+		sim = Sim(self.cpu, list(b"+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-."))
+		sim.run()
+		assert sim.stdout == list(b"hello world")
+
+	def test_59008(self):
+		sim = Sim(self.cpu, list(b"++++[>++++[>++++>+++++++<<-]<-]>>++.>-  (.)(.)  [>+>+<<-]+++[>----<-]>-.>++++."))
+		sim.run()
+		assert sim.stdout == [66, 111, 111, 98, 115]
+
+class TestCPUV1(unittest.TestCase, TestCPUBase):
+	def setUp(self):
+		self.cpu = compile(CPU)
+
+if __name__ == '__main__':
+    unittest.main()
+    #open('Top.dot', 'w').write(generate_dot_file(Top))
+    open('CPU.dot', 'w').write(generate_dot_file(CPU))
 
